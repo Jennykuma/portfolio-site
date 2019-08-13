9bc7110a0557be4bf2fb6e230eb9e10d036665aa
Jenny <jenny.le@ucalgary.ca>
2019-08-12 21:58:04 -0600

    Figured out projects graphQL query and placement of code

---
+++ src/components/project-panel.js
@@ -0,0 +1,57 @@
+import React from "react"
+import { useStaticQuery, Link, graphql, StaticQuery } from "gatsby"
+import styled from 'styled-components'
+
+import DynamicImage from "../components/image"
+
+const Text = styled.text`
+  font-size: 0.9em;
+  line-height: 1.5em;
+`;
+
+const Polaroid = styled.button`
+  position: relative;
+  background: #fff;
+  width: 294px;
+  padding: 0px 0px 15px 0px;
+  margin: 15px;
+  text-align: center;
+  border: 1px solid #f4f4f4;
+
+  &:hover {
+    -moz-box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.10);
+    -webkit-box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.10);
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.10);
+  }
+`;
+
+const ProjectPanel = (props) => (
+  <StaticQuery
+    query={graphql`
+      query projectQuery {
+        dataJson {
+          projectData {
+            project {
+              title
+              imageName
+              description
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        {data.dataJson.projectData.map(projectItem => 
+          <Polaroid>
+            <DynamicImage imageName={projectItem.project.imageName} />
+            <br />
+            <Text> {projectItem.project.description} </Text>
+          </Polaroid>
+        )}
+      </>
+    )}
+  />
+)
+
+export default ProjectPanel;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,12 @@
 import React from "react"
-import { useStaticQuery, Link, graphql } from "gatsby"
+import { StaticQuery, useStaticQuery, Link, graphql } from "gatsby"
 import styled from 'styled-components'
 import { Grid, Col, Row } from 'react-styled-flexboxgrid' 
 
 import DynamicImage from "../components/image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ProjectPanel from "../components/project-panel"
 
 // Create a Title component that'll render an <h1> tag with some styles
 const Name = styled.h1`
@@ -16,42 +17,7 @@ const Name = styled.h1`
   color: #e87461;
 `;
 
-const Text = styled.text`
-  font-size: 0.9em;
-  line-height: 1.5em;
-`;
-
-const Polaroid = styled.button`
-  position: relative;
-  background: #fff;
-  width: 294px;
-  padding: 0px 0px 15px 0px;
-  margin: 15px;
-  text-align: center;
-  border: 1px solid #f4f4f4;
-
-  &:hover {
-    -moz-box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.10);
-    -webkit-box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.10);
-    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.10);
-  }
-`;
-
-const ProjectPanel = (props) => {
-  const projectData = useStaticQuery(graphql`
-    query projectQuery {
-      dataJson {
-        projectData {
-         project {
-           title
-           description
-         }
-       }
-     }
-    }
-  `)
-}
-
+let projectPanelDiv = <ProjectPanel/>
 
 const ProjectsPage = () => (
   <Layout>
@@ -63,15 +29,9 @@ const ProjectsPage = () => (
 
       <Row>
         <Col xs={12} md={12} lg={12} style={{ marginTop: `0%` }}>
-
-          {ProjectPanel.projectData.project.map((projectItem) => 
-            <Polaroid>
-              <DynamicImage imageName={projectItem.imageName} />
-              <br />
-              <Text> {projectItem.description} </Text>
-            </Polaroid>
-          )}
           
+          {projectPanelDiv}
+
         </Col>
       </Row>
     </Grid>
